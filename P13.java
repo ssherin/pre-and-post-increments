52f5e47f1e1c98fbe199049cc6518ce33ff13ebe
ssherin <ssherin044@gmail.com>
2020-01-20 09:25:21 +0530

    commit

---
+++ P13.java
@@ -0,0 +1,7 @@
+class P13{
+	public static void main(String args[]){
+		int x = 5;		
+		int y = x++;						//Solution 1 as "X IS not DECLARED CONSTANT" it can be used in any conversion, 
+		System.out.println("x= " +x+ " y= " +y);		//RESULTS WILL BE SHOWN
+	}	
+}										
